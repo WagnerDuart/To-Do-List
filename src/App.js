96bd7bea9eab98fa2b5696ad212a1ad6775e4c3d
JoaoPauloSouzaSilva <joaopaulloss477@gmail.com>
2024-01-12 16:18:13 -0300

    creating delete

--- src/App.js
+++ src/App.js
@@ -12,33 +12,45 @@ function App() {
     }
   };
 
+  const deleteTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
   return (
-      <>
+    <>
       <div className="background-image"></div>
       <div className="container">
-      <div className="header">
-        <h1>Todo List React JS</h1>
-        <div>
-          <input
-            type="text"
-            id="newTaskInput"
-            value={newTask}
-            onChange={(e) => setNewTask(e.target.value)}
-            placeholder="Tarefa" />
-          <button onClick={addTask} disabled={!newTask.trim()}>
-            ADICIONAR
-          </button>
+        <div className="header">
+          <h1>Todo List React JS</h1>
+          <div>
+            <input
+              type="text"
+              id="newTaskInput"
+              value={newTask}
+              onChange={(e) => setNewTask(e.target.value)}
+              placeholder="Tarefa"
+            />
+            <button onClick={addTask} disabled={!newTask.trim()}>
+              ADICIONAR
+            </button>
+          </div>
+        </div>
+        <div className="task-list">
+          <ul>
+             {tasks.map((task, index) => (
+              <li key={index}>
+                {task}
+                <button className="delete-button" onClick={() => deleteTask(index)}>
+                  <img id='icon' src="https://cdn-icons-png.flaticon.com/256/54/54324.png" alt="lixeira" />
+                </button>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
-      <div className="task-list">
-        <ul>
-          {tasks.map((task, index) => (
-            <li key={index}>{task}</li>
-          ))}
-        </ul>
-      </div>
-    </div></>
-  
+    </>
   );
 }
 
