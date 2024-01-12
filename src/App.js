f544bf089c8b2854bfde4d53325fcd86b969f0ac
JoaoPauloSouzaSilva <joaopaulloss477@gmail.com>
2024-01-12 15:15:32 -0300

    creating to do list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,42 @@
+
+import React, { useState } from 'react';
+import './styles.css';
+
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, newTask]);
+      setNewTask('');
+    }
+  };
+
   return (
-    <h1>Hello World!</h1>
+    <div className="container">
+      <div className="header">
+        <h1>Todo List React JS</h1>
+        <div>
+          <input
+            type="text"
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+            placeholder="Tarefa"
+          />
+          <button onClick={addTask} disabled={!newTask.trim()}>
+            ADICIONAR
+          </button>
+        </div>
+      </div>
+      <div className="task-list">
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>{task}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
 
